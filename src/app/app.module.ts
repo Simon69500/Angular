b452675184b81251cfc598c56108d349a44a4345
Simon69500 <150716564+Simon69500@users.noreply.github.com>
2024-08-30 19:58:17 +0200

    Init Repository

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProposComponent } from './propos/propos.component';
 import { ProduitComponent } from './produit/produit.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProposComponent,
-    ProduitComponent
+    ProduitComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
