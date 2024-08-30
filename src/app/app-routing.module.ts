93fd2928944c23ffd2cf9275ac86b16afb98979d
Simon69500 <150716564+Simon69500@users.noreply.github.com>
2024-08-30 20:09:00 +0200

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { ProduitComponent } from './produit/produit.component';
+import { ProposComponent } from './propos/propos.component';
+
+const routes: Routes = [
+  {path: "", component: HomeComponent},
+  { path:"propos", component:ProposComponent},
+  {path:"produits", component:ProduitComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
